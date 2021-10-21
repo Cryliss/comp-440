70273b13db0ab785fe6aa210d082e1b8166a8b6b
Cryliss <sabrabilodeau@gmail.com>
2021-10-20 19:33:04 -0700

    Added comments

--- server/config.py
+++ server/config.py
@@ -1,6 +1,9 @@
 from app import app
 from flaskext.mysql import MySQL
 
+# Code help: https://webdamn.com/create-restful-api-using-python-mysql/
+# Need to change this to your local SQL information.
+# This should be in an .env file and NOT like this .. TO DO lol.
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'sabra'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'hA#&kfO5Zx8%'
